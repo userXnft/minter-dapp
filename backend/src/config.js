fe667bec92be01587d6c0290fe6a5e1e81cff508
userXnftgit config --global user.email jolly.bolly54@gmail.com <urbangoblinsociety@gmail.comgit config --global user.email>
2022-04-13 10:40:17 +0200

    rinkeby test

--- backend/src/config.js
+++ backend/src/config.js
@@ -61,15 +61,16 @@ const MINT_PRICE = 0.001; // Minting price per NFT. Rinkeby = ETH, Polygon = MAT
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-03-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-05-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-04-12T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xC318bB53Adfe9bD2749096BD89a79D79073aEbCA"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = ["0xC318bB53Adfe9bD2749096BD89a79D79073aEbCA" , "0xd5b2E44Ca5dDEaA98a66D86520A9dFfb5922a22f"];
+const PRESALE_WHITELISTED_ADDRESSES = ["0x9ea14ba02CfBaE724408b662E85F577F41225ed6"];
+
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
